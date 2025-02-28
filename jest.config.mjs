b8bd5d303b108ddb95b141381e1f4bb65e00c4f4
BUR4KBEY <rectfc@gmail.com>
2025-02-28 22:07:41 +0300

    chore: convert jest config to ESM

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-// eslint-disable-next-line no-undef
-module.exports = {
+export default {
   setupFilesAfterEnv: ['jest-extended/all'],
   testEnvironment: 'node',
   preset: 'ts-jest'
